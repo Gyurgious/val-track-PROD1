39708d242779fc0d29a984793d8546c18770d1d5
Quang Le <106447805+Gyurgious@users.noreply.github.com>
2024-10-28 15:44:34 -0500

    overview page
    - added more styling
    - created avg stats section
    - wrote more view functions

--- backend/val_api/views.py
+++ backend/val_api/views.py
@@ -122,6 +122,14 @@ def get_processed_data(request):
         processed_data = []
         charac = "./photos/omen-1.png"
 
+
+        # calculate average stats
+        user_avg_hs = 0
+        user_total_wins = 0
+        user_total_deaths = 0
+        user_total_kills = 0
+
+
         for match in match_data['data']:
             user_map = match['metadata']['map']
             user_mode = match['metadata']['mode']
@@ -145,10 +153,13 @@ def get_processed_data(request):
 
                     user_total_shots = user_hs + user_bs + user_ls
                     user_hs_percentage = round(user_hs/user_total_shots, 2) * 100
-                    
-                    print(user_team)
+
+                    user_avg_hs += user_hs_percentage
+                    user_total_kills += user_kills
+                    user_total_deaths += user_deaths
+
                     break;
-    
+
             if user_team:
                 if user_team == "Red":
                         user_rounds_won = match['teams']['red']['rounds_won']
@@ -157,6 +168,14 @@ def get_processed_data(request):
                     user_rounds_won = match['teams']['blue']['rounds_won']
                     opponent_rounds_won = match['teams']['red']['rounds_won']
 
+                user_wins = 0
+                if (user_rounds_won > opponent_rounds_won):
+                    user_total_wins += 1
+
+                
+                
+                print("user hs is " + str(user_avg_hs))
+
                 processed_data.append({
                     'score': f"{user_rounds_won} - {opponent_rounds_won }",
                     'team': user_team,
@@ -164,12 +183,112 @@ def get_processed_data(request):
                     'time_start': user_match_start,
                     'mode': user_mode,
                     'kills': user_kills,
+                    'assists': user_assists,
+                    'deaths':user_deaths,
                     'headshot_percentage': user_hs_percentage,
                     'agent': user_agent,
                     'Omen': "/agent-photos/omen-1.png"
                 })
 
+
+        # avg wr, avg KDA, avg HS%
+        user_avg_hs_percentage = round(user_avg_hs/5, 2)
+        user_avg_wr = round(user_total_wins/5, 2) * 100
+        user_avg_kd = round(user_total_kills/user_total_deaths, 2)
+
+
         return JsonResponse({'data': processed_data}, safe=False)
+        
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({"error": str(e)}, status=response.status_code)
+    
+
+def get_avg_data(request):
+    user = request.GET.get('user', '')
+    tag = request.GET.get('tag', '')
+
+    match_api = f"https://api.henrikdev.xyz/valorant/v3/matches/na/{user}/{tag}"
+
+    headers = {
+        'Authorization': API_KEY,
+    }
+
+    try:
+        response = requests.get(match_api, headers=headers)
+        response.raise_for_status()
+        match_data = response.json()
+
+
+      
+        processed_data = []
+        charac = "./photos/omen-1.png"
+
+
+        # calculate average stats
+        user_avg_hs = 0
+        user_total_wins = 0
+        user_total_deaths = 0
+        user_total_kills = 0
+
+
+        for match in match_data['data']:
+            user_map = match['metadata']['map']
+            user_mode = match['metadata']['mode']
+            user_match_start = match['metadata']['game_start_patched'] # fix time(late by 6 hours)
+            user_team = None
+            user_rounds_won = 0
+            opponent_rounds_won = 0
+
+
+
+            for player in match['players']['all_players']:
+                if player['name'] == user:
+                    user_team = player['team']
+                    user_kills = player['stats']['kills']
+                    user_assists = player['stats']['assists']
+                    user_deaths = player['stats']['deaths']
+                    user_agent = player['character']
+                    user_hs = player['stats']['headshots']
+                    user_bs = player['stats']['bodyshots']
+                    user_ls = player['stats']['legshots']
+
+                    user_total_shots = user_hs + user_bs + user_ls
+                    user_hs_percentage = round(user_hs/user_total_shots, 2) * 100
+
+                    user_avg_hs += user_hs_percentage
+                    user_total_kills += user_kills
+                    user_total_deaths += user_deaths
+
+                    break;
+
+            if user_team:
+                if user_team == "Red":
+                        user_rounds_won = match['teams']['red']['rounds_won']
+                        opponent_rounds_won = match['teams']['blue']['rounds_won']
+                else:
+                    user_rounds_won = match['teams']['blue']['rounds_won']
+                    opponent_rounds_won = match['teams']['red']['rounds_won']
+
+                user_wins = 0
+                if (user_rounds_won > opponent_rounds_won):
+                    user_total_wins += 1
+     
+
+
+        # avg wr, avg KDA, avg HS%
+        user_avg_hs_percentage = round(user_avg_hs/5, 2)
+        user_avg_wr = round(user_total_wins/5, 2) * 100
+        user_avg_kd = round(user_total_kills/user_total_deaths, 2)
+
+        print(user_avg_wr)
+
+        data = { # single dictionary
+            'user_hs': user_avg_hs_percentage,
+            'user_wr': user_avg_wr,
+            'user_kd': user_avg_kd,
+        }
+
+        return JsonResponse(data)
         
     except requests.exceptions.RequestException as e:
         return JsonResponse({"error": str(e)}, status=response.status_code)
